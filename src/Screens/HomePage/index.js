17e13f3275280ca8c014b44b706b5e3c447c47b6
ten-3dev <mj03dev@gmail.com>
2022-01-31 13:32:07 +0900

    Main Banner working..

--- src/Screens/HomePage/index.js
+++ src/Screens/HomePage/index.js
@@ -1,18 +1,50 @@
 import React from "react";
+import * as Styles from "./style";
 import Header from "../../Components/Header";
 import Category from "../../Components/Category";
 import BottomSignIn from "../../Components/BottomSignIn";
 import Footer from "../../Components/Footer";
-import * as Styles from "./style";
-// import { BannerArrow } from "../../Assets";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const NextArrow = props => {
+  const { className, onClick } = props;
+  return <Styles.BannerArrow className={className} onClick={onClick} />;
+};
+
+const PrevArrow = props => {
+  const { className, onClick } = props;
+  return <Styles.BannerArrow className={className} onClick={onClick} />;
+};
 
 const HomePage = () => {
+  const BannerList = [
+    "https://m.media-amazon.com/images/I/61DUO0NqyyL._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/61nfpX0p23L._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/61rSRZL9kaL._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/610aFo74RdL._SX3000_.jpg"
+  ];
+  const settings = {
+    infinite: true,
+    speed: 200,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
+  };
+
   return (
     <>
       <Header />
       <Category />
       <Styles.HomePageBackground>
-        <Styles.BannerBackground></Styles.BannerBackground>
+        <Styles.BannerBackground>
+          <Styles.SliderCustom {...settings}>
+            {BannerList.map((url, idx) => {
+              return <Styles.BannerItem img={url} key={idx} />;
+            })}
+          </Styles.SliderCustom>
+        </Styles.BannerBackground>
       </Styles.HomePageBackground>
       <BottomSignIn />
       <Footer />
--- src/Screens/HomePage/style.js
+++ src/Screens/HomePage/style.js
@@ -1,11 +1,66 @@
 import styled from "styled-components";
+import Slider from "react-slick";
 
 export const HomePageBackground = styled.div``;
 
 export const BannerBackground = styled.div`
-  width: 100%;
-  height: 600px;
+  width: 1500px;
+  margin: 0px auto;
   text-align: center;
   white-space: nowrap;
-  // overflow: hidden;
+`;
+
+export const SliderCustom = styled(Slider)`
+  .slick-slide div {
+    outline: none;
+  }
+  .slick-next {
+    width: 34px;
+    height: 52px;
+    /* width: 80px;
+    height: 280px; */
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url("https://m.media-amazon.com/images/S/sash/ydDD9hnRfziI$y7.png");
+    transform: translate(0, -50%) rotate(180deg);
+    top: 125px;
+    z-index: 1;
+    right: 20px;
+  }
+  .slick-prev {
+    width: 34px;
+    height: 52px;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-image: url("https://m.media-amazon.com/images/S/sash/ydDD9hnRfziI$y7.png");
+    top: 125px;
+    z-index: 1;
+    left: 20px;
+  }
+  .slick-next::before {
+    display: none;
+  }
+  .slick-prev::before {
+    display: none;
+  }
+`;
+
+export const BannerItem = styled.div`
+  width: 100%;
+  height: 600px;
+  background-size: cover;
+  background-image: linear-gradient(transparent, transparent, white),
+    ${props => {
+      if (props.img) return `url(${props.img})`;
+    }};
+`;
+
+export const BannerImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
+
+export const BannerArrow = styled.div`
+  display: "block";
 `;
